6de78dd93874659dbd4ffbef2f380da910f0fa12
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-08-28 07:29:01 +0530

    chore: Update page.tsx to include HSBC in the Financial Dashboard heading

--- hsbc-sol/src/app/layout.tsx
+++ hsbc-sol/src/app/layout.tsx
@@ -8,8 +8,8 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HSBC HSBC",
-  description: "A HSBC for HSBC",
+  title: "HSBC Financial Dashboard",
+  description: "A HSBC for Financial Dashboard",
 };
 
 export default function RootLayout({
